c5a4737233990c07de7f8854ed6ec0cbcff707c0
Skye Viau <skye.viau@gmail.com>
2019-10-25 14:59:24 -0400

    Base methods.
    Signed-off-by: Skye Viau <skye.viau@gmail.com>

--- src/gq/skye/todoTxt/Main.java
+++ src/gq/skye/todoTxt/Main.java
@@ -1,7 +1,54 @@
 package gq.skye.todoTxt;
 
+import java.util.ArrayList;
+
+/**
+ * Main class.
+ */
+
 public class Main {
+
+    /**
+     * Default location of the todo.txt file.
+     */
+
+    static String todoLocation;
+
+    /**
+     * Main Class
+     * @param args (Optional) Location of todo.txt file.
+     */
+
     public static void main(String[] args) {
+        if (!(args[0].isEmpty())) {
+            todoLocation = args[0];
+        }
+        loadTodo();
+    }
+
+    /**
+     * Load the todo object from a file.
+     * @return Todo object.
+     */
+
+    static ArrayList loadTodo() {
+
+    }
+
+    /**
+     * Save the todo object.
+     * @return Status of save.
+     */
+
+    static int saveTodo() {
+
+    }
+
+    /**
+     * Display todo file
+     */
+
+    static void displayTodo() {
 
     }
 }
