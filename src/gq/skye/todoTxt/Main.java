c5622b2675d76e3e0d3980b6243afaa5de2e3754
Skye Viau <skye.viau@gmail.com>
2019-10-30 02:01:02 -0400

    Load and display file
    Signed-off-by: Skye Viau <skye.viau@gmail.com>

--- src/gq/skye/todoTxt/Main.java
+++ src/gq/skye/todoTxt/Main.java
@@ -1,5 +1,7 @@
 package gq.skye.todoTxt;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 /**
@@ -12,27 +14,45 @@ public class Main {
      * Default location of the todo.txt file.
      */
 
-    static String todoLocation;
+    static String todoLocation = System.getProperty("user.home") + "/todo.txt";
+
+    /**
+     * Storage for todo objects
+     */
+
+    static ArrayList<String> todoObjects = new ArrayList<>();
 
     /**
      * Main Class
-     * @param args (Optional) Location of todo.txt file.
+     * @param args (Optional) Location of todo.txt file. Must be exact filepath
      */
 
-    public static void main(String[] args) {
-        if (!(args[0].isEmpty())) {
+    public static void main(String[] args) throws IOException {
+        if (!(args.length == 0)) {
             todoLocation = args[0];
         }
         loadTodo();
+        displayTodo();
     }
 
     /**
      * Load the todo object from a file.
-     * @return Todo object.
      */
 
-    static ArrayList loadTodo() {
-
+    static void loadTodo() throws IOException {
+        try {
+            BufferedReader inFile = new BufferedReader(new FileReader(todoLocation));
+            String str;
+            while ((str = inFile.readLine()) != null) {
+                todoObjects.add(str);
+            }
+        } catch (FileNotFoundException e) {
+            PrintWriter writer = new PrintWriter(todoLocation, StandardCharsets.UTF_8);
+            writer.close();
+            loadTodo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -41,7 +61,7 @@ public class Main {
      */
 
     static int saveTodo() {
-
+        return 1;
     }
 
     /**
@@ -49,6 +69,8 @@ public class Main {
      */
 
     static void displayTodo() {
-
+        for (int i = 0; i < todoObjects.size(); i++) {
+            System.out.println((i + 1) + " - " + todoObjects.get(i));
+        }
     }
 }
